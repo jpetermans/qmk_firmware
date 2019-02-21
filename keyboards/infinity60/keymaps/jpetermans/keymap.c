7d06cccacdd1b10687ea23da5689ee70e7e105f6
Joel Petersen <tibcmhhn@gmail.com>
2019-02-21 14:01:43 -0800

    initial attempt to add macros to keymap.c

--- keyboards/infinity60/keymaps/jpetermans/keymap.c
+++ keyboards/infinity60/keymaps/jpetermans/keymap.c
@@ -1,5 +1,8 @@
-#include QMK_KEYBOARD_H
+#include "infinity60.h"
 #include "led_controller.h"
+
+//Helpful Defines
+#define _______ KC_TRNS
 
 //Define Layer Names
 #define _BASE 0
@@ -7,6 +10,15 @@
 #define _FNAV 2
 #define _MEDIA 3
 #define _TILDE 4
+
+/*
+//Shorten dynamic macro names
+#define DMPLAY1 DYN_MACRO_PLAY1
+#define DMPLAY2 DYN_MACRO_PLAY2
+#define DMREC1 DYN_REC_START1
+#define DMREC2 DYN_REC_START2
+#define DMSTOP DYN_REC_STOP
+*/
 
 //IS31 chip has 8 available led pages, using 0 for all leds and 7 for single toggles
 #define max_pages 6
@@ -25,7 +37,10 @@ enum ic60_keycodes {
   MODE_SINGLE,
   MODE_PAGE,
   MODE_FLASH
+//DYNAMIC_MACRO_RANGE
 };
+
+//#include "dynamic_macro.h"
 
 uint8_t current_layer_global = 0;
 uint8_t led_mode_global = MODE_SINGLE;
@@ -51,7 +66,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     /* default */
-    [_BASE] = LAYOUT_60_ansi_split_bs_rshift( \
+    [_BASE] = KEYMAP( \
         KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_NO,\
         KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,   \
         TT(_FNAV), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,         \
@@ -60,7 +75,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /* numpad */
-    [_NUMPAD] = LAYOUT_60_ansi_split_bs_rshift( \
+    [_NUMPAD] = KEYMAP( \
         _______,_______,_______,_______,_______,_______,_______, KC_P7, KC_P8, KC_P9, KC_PSLS, _______,_______,_______,KC_NO,\
         _______,_______,_______,_______,_______,_______,_______, KC_P4, KC_P5, KC_P6, KC_PAST, _______,_______,_______, \
         MO(_FNAV),_______,_______,_______,_______,_______,_______, KC_P1, KC_P2, KC_P3, KC_PMNS, _______,_______,      \
@@ -69,16 +84,16 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /* F-, arrow, and media keys */
-    [_FNAV] = LAYOUT_60_ansi_split_bs_rshift( \
+    [_FNAV] = KEYMAP( \
         KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,KC_NO,\
         KC_CAPS,_______,_______,_______,_______,_______,_______,KC_PGUP,KC_UP,KC_PGDN,KC_PSCR,_______,_______,KC_DEL,  \
         _______,_______,KC_BTN2,_______,_______,_______,KC_HOME,KC_LEFT,KC_DOWN,KC_RGHT,KC_INS,_______,_______,     \
-        _______,KC_APP,KC_BTN1,KC_CALC,_______,_______,KC_END,_______,_______,_______,_______,_______,KC_NO,         \
+        _______,KC_APP, KC_BTN1,KC_CALC,_______,_______,KC_END,_______,_______,_______,_______,_______,KC_NO,         \
         _______,_______,_______,               _______,         F(CTLALTDEL),KC_NLCK,_______,_______   \
     ),
 
     /* media */
-    [_MEDIA] = LAYOUT_60_ansi_split_bs_rshift( \
+    [_MEDIA] = KEYMAP( \
         _______,F(MODE_SINGLE),F(MODE_PAGE),F(MODE_FLASH),_______,_______,_______, _______, _______, _______,KC_MUTE, KC_VOLD, KC_VOLU,_______,KC_NO,\
         _______,_______,_______,_______,_______,_______,_______, _______, _______, _______,_______, _______,_______,_______,\
         _______,_______,_______,_______,_______,F(GAME),_______, _______, _______, _______,_______, _______,_______,     \
@@ -86,7 +101,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,               KC_MPLY,             _______,_______, _______,_______      \
     ),
     /* ~ */
-    [_TILDE] = LAYOUT_60_ansi_split_bs_rshift( \
+    [_TILDE] = KEYMAP( \
         KC_GRV,_______,_______,_______,_______,_______,_______, _______, _______, _______,_______, _______,_______,_______,KC_NO,\
         _______,_______,_______,_______,_______,_______,_______, _______, _______, _______,_______, _______,_______,_______,\
         _______,_______,_______,_______,_______,_______,_______, _______, _______, _______,_______, _______,_______,     \
@@ -94,7 +109,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,               _______,             _______,_______, _______,_______      \
     ),
     /* template */
-    [5] = LAYOUT_60_ansi_split_bs_rshift( \
+    [5] = KEYMAP( \
         _______,_______,_______,_______,_______,_______,_______, _______, _______, _______,_______, _______,_______,_______,KC_NO,\
         _______,_______,_______,_______,_______,_______,_______, _______, _______, _______,_______, _______,_______,_______,\
         _______,_______,_______,_______,_______,_______,_______, _______, _______, _______,_______, _______,_______,     \
